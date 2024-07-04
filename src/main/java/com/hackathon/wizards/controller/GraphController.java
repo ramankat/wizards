523843f6dc96e2a97b355bfa01fc15aed00ea61d
Raman Kataria <raman.kataria@dpworld.com>
2024-07-04 16:22:32 +0530

    added cors

--- src/main/java/com/hackathon/wizards/controller/GraphController.java
+++ src/main/java/com/hackathon/wizards/controller/GraphController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="v1/graph")
+@CrossOrigin(origins = "/*")
 @Slf4j
 public class GraphController {
 
